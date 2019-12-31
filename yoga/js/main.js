2b2fdce80b5316cc49706d62020e9fd8447ae07e
Almaz <almazzzik@yandex.ru>
2019-12-31 17:00:51 +0300

    add timer

--- yoga/js/main.js
+++ yoga/js/main.js
@@ -3,11 +3,11 @@ window.addEventListener('DOMContentLoaded', function () {
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
-        
+
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');            
+            tabContent[i].classList.add('hide');
         }
     }
 
@@ -21,16 +21,63 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     info.addEventListener('click', function (event) {
-       let target = event.target;
-       if(target && target.classList.contains('info-header-tab')){
-           for (let i = 0; i < tab.length; i++) {
-               if(target == tab[i]){
-                   hideTabContent(0);
-                   showTabContent(i);
-                   break;
-               }               
-               
-           }
-       } 
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+
+            }
+        }
     });
+    //Timer
+
+    let deadline = '2020-01-01';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            // hours = Math.floor((t / (1000 * 60 * 60)));
+            hours = Math.floor((t / 1000 / 60) % 24),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = minutes.textContent = seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
