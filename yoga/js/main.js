5c73583ffcf79778ebb65794ad763fbe08433d1c
Almaz <almazzzik@yandex.ru>
2020-01-01 17:45:49 +0300

    learned ajax method and json

--- yoga/js/main.js
+++ yoga/js/main.js
@@ -34,14 +34,14 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     //Timer
-    let deadline = '2020-01-01';
+    let deadline = '2020-01-02';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t / 1000) % 60),
             minutes = Math.floor((t / 1000 / 60) % 60),
-            // hours = Math.floor((t / (1000 * 60 * 60)));
-            hours = Math.floor((t / 1000 / 60) % 24),
+            hours = Math.floor((t / (1000 * 60 * 60))),
+            // hours = Math.floor((t / 1000 / 60) % 24),
             days = Math.floor((t / (1000 * 60 * 60 * 24)));
         return {
             'total': t,
@@ -107,6 +107,82 @@ window.addEventListener('DOMContentLoaded', function () {
             document.body.style.overflow = 'hidden';
         });
     });
+
+    //Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if(request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            }else if(request.readyState === 4 && request.status == 200){
+                statusMessage.innerHTML = message.success;
+            }else{
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';            
+        }
+    });
+
+    let contactForm = document.querySelector('#form');
+
+    contactForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        contactForm.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(contactForm);
+
+        let obj = {};
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if(request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            }else if(request.readyState === 4 && request.status == 200){
+                statusMessage.innerHTML = message.success;
+            }else{
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 1; i < input.length; i++) {
+            input[i].value = '';            
+        }
+    });
 });
-
-
