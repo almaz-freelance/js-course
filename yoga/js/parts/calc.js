df08a4746cbdf797e3bf5e02572d13b5af911617
Almaz <almazzzik@yandex.ru>
2020-01-03 01:44:12 +0300

    working with webpack

---
+++ yoga/js/parts/calc.js
@@ -0,0 +1,43 @@
+function calc() {
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personSum = 0,
+        daySum = 0,
+        total = 0;
+
+    totalValue.textContent = 0;
+
+    persons.addEventListener('change', function () {
+        personSum = +this.value;
+        total = (daySum + personSum) * 4000;
+
+        if (restDays.value == '' || this.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+    restDays.addEventListener('change', function () {
+        daySum = +this.value;
+        total = (daySum + personSum) * 4000;
+
+        if (persons.value == '' || this.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            let a = total;
+            totalValue.textContent = a * this.options[this.selectedIndex].value;
+        }
+    });
+}
+
+module.exports = calc;
---
+++ yoga/js/parts/modal.js
@@ -0,0 +1,29 @@
+function modal() {
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splah');
+        document.body.style.overflow = '';
+    });
+
+    let moreTab = document.querySelectorAll('.description-btn');
+
+    moreTab.forEach(function (item) {
+        item.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+}
+
+module.exports = modal;
